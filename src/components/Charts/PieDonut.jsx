c05f92ee5da1ce127f83d497b9da624b9b90e174
01134K <chonthanawat.k@ku.th>
2023-06-05 18:07:46 +0700

    add : Line and Scatter Charts

--- src/components/Charts/PieDonut.jsx
+++ src/components/Charts/PieDonut.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import Chart from "react-apexcharts";
 
-//  const series = [44, 55, 41, 17, 15
  const options = {
  chart: {
  type: "donut"
  },
-//  labels: ['Apple', 'Mango', 'Orange', 'Watermelon',"dss"]
-labels : []
+ colors: [  '#C0392B', '#E74C3C', '#9B59B6',
+            '#8E44AD', '#2980B9', '#3498DB',
+            '#1ABC9C', '#16A085', '#27AE60'
+        ],
+ labels : [],
+ tooltip: {
+    y: {
+      formatter: function (val) {
+        return "$ " + val + " thousands"
+      }
+    }
+  }
  };
 
  export default function PieDonut({series,L}) {
     // options.labels = labels
+    // options.labels = [];
     options.labels = L;
  return (
   <div>
--- src/routes/std/Charts_pages.jsx
+++ src/routes/std/Charts_pages.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import PieDonut from '../../components/Charts/PieDonut'
 import Bar from '../../components/Charts/Bar_Basic'
+import Line_ from '../../components/Charts/Line_Chart';
+import Scatter_Charts from '../../components/Charts/Scatter_Charts';
 function Charts_pages() {
     const series= [{
         name: 'Net Profit',
         data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
-        labels: ['A1', 'B1', 'C1', 'D1',"E1", "F1", "G1","H1", "I1"]
+        labels: ['Feb 1', 'Ma 1', 'Apr 1', 'May 1', 'Jun 1', 'Jul 1', 'Aug 1', 'Sep 1', 'Oct 1']
       }, {
         name: 'Revenue',
         data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
-        labels: ['A2', 'B2', 'C2', 'D2',"E2", "F2", "G2","H2", "I2"]
+        labels: ['Feb 2', 'Mar 2', 'Apr 2', 'May 2', 'Jun 2', 'Jul 2', 'Aug 2', 'Sep 2', 'Oct 2']
       }, {
         name: 'Free Cash Flow',
         data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
-        labels: ['A3', 'B3', 'C3', 'D3',"E3", "F3", "G3","H3", "I3"]
+        labels: ['Feb 3', 'Mar 3', 'Apr 3', 'May 3', 'Jun 3', 'Jul 3', 'Aug 3', 'Sep 3', 'Oct 3']
       }];
 
     const navigate = useNavigate()
@@ -38,9 +40,17 @@ function Charts_pages() {
                 </div>
                 
                 
-                <div class=" p-8 rounded overflow-hidden shadow-lg">
-                    <Bar series = {series}/>           
+                <div class="flex" >
+                    <div class=" p-8 rounded overflow-hidden shadow-lg">
+                        <Bar series = {series}/>                  
+                    </div>
+                    <div class=" p-8 rounded overflow-hidden shadow-lg">      
+                        <Line_ series = {series}/>     
+                    </div>
                 </div>
+                {/* <div class=" p-8 rounded overflow-hidden shadow-lg">      
+                        <Scatter_Charts series = {series}/>     
+                </div> */}
             </div>
         </div>
     )
