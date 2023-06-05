090b1cbf50aa2c3e2adea3bcc493f7e36275c647
01134K <chonthanawat.k@ku.th>
2023-06-05 14:43:23 +0700

    add : bar chart

--- src/components/Charts/PieDonut.jsx
+++ src/components/Charts/PieDonut.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Chart from "react-apexcharts";
 
- const series = [44, 55, 41, 17, 15];
+//  const series = [44, 55, 41, 17, 15
  const options = {
  chart: {
  type: "donut"
- }
+ },
+//  labels: ['Apple', 'Mango', 'Orange', 'Watermelon',"dss"]
+labels : []
  };
 
- export default function PieDonut() {
+ export default function PieDonut({series,L}) {
+    // options.labels = labels
+    options.labels = L;
  return (
   <div>
   <Chart options={options} series={series} type="donut" height={300} />
--- src/routes/std/Charts_pages.jsx
+++ src/routes/std/Charts_pages.jsx
@@ -1,15 +1,46 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import PieDonut from '../../components/Charts/PieDonut'
-
+import Bar from '../../components/Charts/Bar_Basic'
 function Charts_pages() {
+    const series= [{
+        name: 'Net Profit',
+        data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+        labels: ['A1', 'B1', 'C1', 'D1',"E1", "F1", "G1","H1", "I1"]
+      }, {
+        name: 'Revenue',
+        data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+        labels: ['A2', 'B2', 'C2', 'D2',"E2", "F2", "G2","H2", "I2"]
+      }, {
+        name: 'Free Cash Flow',
+        data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+        labels: ['A3', 'B3', 'C3', 'D3',"E3", "F3", "G3","H3", "I3"]
+      }];
+
     const navigate = useNavigate()
     return (
-        <div>
-            <h1>hello</h1>
-            <div>
-                <PieDonut/> 
-                   
+        <div class="grid justify-items-center">
+            <h1>hello this page is Dasbord</h1>
+            <div class="grid justify-items-center">
+                <div class="flex">
+                    <div class="p-8 rounded overflow-hidden shadow-lg ">
+                        <h1>{series[0].name}</h1>
+                        <PieDonut series = {series[0].data} L = {series[0].labels}/>           
+                    </div>
+                    <div class="p-8 rounded overflow-hidden shadow-lg ">
+                        <h1>{series[1].name}</h1>
+                        <PieDonut series = {series[1].data} L = {series[1].labels}/>           
+                    </div>
+                    <div class="p-8 rounded overflow-hidden shadow-lg ">
+                        <h1>{series[2].name}</h1>
+                        <PieDonut series = {series[2].data} L = {series[2].labels}/>           
+                    </div>
+                </div>
+                
+                
+                <div class=" p-8 rounded overflow-hidden shadow-lg">
+                    <Bar series = {series}/>           
+                </div>
             </div>
         </div>
     )
